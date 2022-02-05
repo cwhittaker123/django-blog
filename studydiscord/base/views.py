9585100ba2af9a97c461bc0059b8073858cae4c1
cwhittaker123 <chriswhittaker123@gmail.com>
2022-02-05 13:55:59 -0500

    Separated templates for main application and base application

--- studydiscord/base/views.py
+++ studydiscord/base/views.py
@@ -2,8 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 
 # Create your views here.
+
+rooms = [
+    {'id': 1, 'name': 'Learn Python'},
+    {'id': 2, 'name': 'Design'},
+    {'id': 3, 'name': 'Frontend Devs'},
+]
+
 def home(request : HttpRequest):
-    return render(request, 'home.html')
+    # Access room variable
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
 def room(request : HttpRequest):
-    return render(request, 'room.html')
+    return render(request, 'base/room.html')
