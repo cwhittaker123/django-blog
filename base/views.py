a5c165a7c41fc2c9bda8c99f3341b2c60a0598a8
cwhittaker123 <chriswhittaker123@gmail.com>
2022-02-05 17:21:15 -0500

    Updating views to use the rooms from the db

--- base/views.py
+++ base/views.py
@@ -1,25 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
+from base.models import Room
 
 # Create your views here.
 # Views contain logic and inject dynamic data into rendered templates
 # They receive an HttpRequest and return a HttpResponse
 
-rooms = [
-    {'id': 1, 'name': 'Learn Python'},
-    {'id': 2, 'name': 'Design'},
-    {'id': 3, 'name': 'Frontend Devs'},
-]
+# rooms = [
+#     {'id': 1, 'name': 'Learn Python'},
+#     {'id': 2, 'name': 'Design'},
+#     {'id': 3, 'name': 'Frontend Devs'},
+# ]
 
 def home(request : HttpRequest) -> HttpResponse:
+    # queryset = ModelName.objects.all()/.get()/.filter()/.exclude()
+    # get all rooms in the db
+    rooms = Room.objects.all()
     # Access rooms variable
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 def room(request : HttpRequest, pk : str) -> HttpResponse:
-    room = None
-    for rm in rooms:
-        if rm.get('id') == int(pk):
-            room = rm
+    room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
