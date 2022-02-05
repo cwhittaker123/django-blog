2f0c57896c7309320fe9810835b055d1579feaf4
cwhittaker123 <chriswhittaker123@gmail.com>
2022-02-05 14:42:33 -0500

    Access path url by using name variable in template

--- studydiscord/base/urls.py
+++ studydiscord/base/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from base import views
 
 urlpatterns = [
+    # Access the path by using the name supplied
+    # Use a name so that the urls can change and we don't have to change them throughout the app
     path('', views.home, name="home"),
     path('room/<str:pk>', views.room, name="room"), # dynamic value of type str <str:pk>
 ]
